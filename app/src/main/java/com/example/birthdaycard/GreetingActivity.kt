43521be2a04ccbc3149f912da1b4fde573ad4b6c
unknown <bossrc64@outlook.com>
2024-03-03 16:03:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/birthdaycard/GreetingActivity.kt
@@ -0,0 +1,18 @@
+package com.example.birthdaycard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.birthdaycard.databinding.ActivityGreetingBinding
+import com.example.birthdaycard.databinding.ActivityMainBinding
+
+class GreetingActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityGreetingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityGreetingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val birthdayMessage = getString(R.string.happy_birthday) + " " + intent.getStringExtra("NAME_EXTRA")
+        binding.happyBirthdayText.text = birthdayMessage
+    }
+}
---
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.birthdaycard
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.birthdaycard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.generateButton.setOnClickListener {
+            val name = binding.nameTextField.text.toString()
+            val greetingIntent = Intent(this, GreetingActivity::class.java).apply {
+                putExtra("NAME_EXTRA", name)
+                Log.d("Name log", name)
+            }
+            startActivity(greetingIntent)
+        }
+    }
+}
